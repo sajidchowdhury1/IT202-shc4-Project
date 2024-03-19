2a9ea971f98f5b6ce9a83c9255ea6aaca4c996ce
sajidchowdhury1 <shc4@njit.edu>
2024-03-19 12:31:20 -0400

    Applied css to the create form page but also need to do some testing before submission

--- create_form.php
+++ create_form.php
@@ -26,19 +26,19 @@
     </head>
     <body>
         <?php include('header.php');?>
-        <main>
+        <main id="createpage">
             <h2>Create Product</h2>
 
             <?php 
                 if(isset($error_message)){
-                    echo '<p>' . $error_message . '</p>';
+                    echo '<p style="color: red; font-weight: bold; font-size: 15px; text-align: center;">' . $error_message . '</p>';
                 }
                 if(isset($success_message)){
-                    echo '<p>' . $success_message . '</p>';
+                    echo '<p style="color: green; font-weight: bold; font-size: 15px; text-align: center;">' . $success_message . '</p>';
                 }
             ?>
 
-            <form action="create_process.php" method="post">
+            <form id="createform" action="create_process.php" method="post">
                 <label>Category:</label>
                 <select name="category_id">
                     <?php foreach ($categories as $category) : ?>
@@ -50,37 +50,37 @@
                 <br />
 
                 <label>Product Code:</label>
-                <input type="text" name="product_code" placeholder="Ex. light21" value="<?php echo htmlspecialchars($product_code)?>" />
+                <input type="text" name="product_code" placeholder="Ex. light21" value="<?php echo htmlspecialchars($product_code);?>" />
                 <br />
 
                 <label>Product Name:</label>
-                <input type="text" name="product_name" placeholder="Ex. smart light" value="<?php echo htmlspecialchars($product_name)?>" />
+                <input type="text" name="product_name" placeholder="Ex. smart light" value="<?php echo htmlspecialchars($product_name);?>" />
                 <br />
 
                 <label for="description">Description:</label>
                 <br />
-                <textarea name="description" placeholder="this product has..." value="<?php echo htmlspecialchars($description)?>"></textarea>
+                <textarea name="description" placeholder="this product has..." value="<?php echo htmlspecialchars($description);?>"></textarea>
                 <br />
 
                 <label>Product Color:</label>
-                <input type="text" name="product_color" placeholder="Ex. red" value="<?php echo htmlspecialchars($product_color)?>" />
+                <input type="text" name="product_color" placeholder="Ex. red" value="<?php echo htmlspecialchars($product_color);?>" />
                 <br />
 
                 <label>Price:</label>
-                <input type="number" step="0.01" name="product_price" placeholder="100.00" value="<?php echo htmlspecialchars($product_price)?>">
+                <input type="number" step="0.01" name="product_price" placeholder="100.00" value="<?php echo htmlspecialchars($product_price);?>">
+                <br />
                 <br />
 
                 <input type="submit" value="Submit">
 
             </form>
 
-            <div>
-                <form action="create_form.php">
-                    <label>Clear Form:</label>
-                    <br />
-                    <input type="submit" value="Restart">
-                </form>
-            </div>
+            
+            <form id="clear" action="create_form.php" method="post">
+                <label>Clear Form:</label>
+                <br />
+                <input type="submit" value="Clear">
+            </form>
 
         </main>
         <?php include('footer.php');?>
--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
 <!-- Sajid Chowdhury Updated: Feb 26, 2024 IT202-006 Phase 2 shc4@njit.edu-->
 <footer>
     <hr />
-    <h5><p>Sajid Chowdhury, Feb 26 2024, IT202-006, shc4@njit.edu</p><h5>
+    <h5><p>Sajid Chowdhury, Mar 18 2024, IT202-006, shc4@njit.edu</p><h5>
 </footer>
