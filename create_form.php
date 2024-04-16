008c401bc829fb5d3f72187113b4e239176afbf0
sajidchowdhury1 <shc4@njit.edu>
2024-04-15 20:18:16 -0400

    Made validations for create page and now need to work on some css

--- create_form.php
+++ create_form.php
@@ -58,39 +58,110 @@
                 <br />
 
                 <label>Product Code:</label>
-                <input type="text" name="product_code" placeholder="Ex. light21" value="<?php echo htmlspecialchars($product_code);?>" />
+                <span id="code">*</span>
+                <input type="text" id="product_code" name="product_code" placeholder="Ex. light21" value="<?php echo htmlspecialchars($product_code);?>" />
                 <br />
 
                 <label>Product Name:</label>
-                <input type="text" name="product_name" placeholder="Ex. smart light" value="<?php echo htmlspecialchars($product_name);?>" />
+                <span id="name">*</span>
+                <input type="text" id="product_name" name="product_name" placeholder="Ex. smart light" value="<?php echo htmlspecialchars($product_name);?>" />
                 <br />
 
                 <label for="description">Description:</label>
+                <span id="text">*</span>
                 <br />
-                <textarea name="description" placeholder="this product has..." value="<?php echo htmlspecialchars($description);?>"></textarea>
+                <textarea id="description" name="description" placeholder="this product has..." value="<?php echo htmlspecialchars($description);?>"></textarea>
                 <br />
 
                 <label>Product Color:</label>
-                <input type="text" name="product_color" placeholder="Ex. red" value="<?php echo htmlspecialchars($product_color);?>" />
+                <span id="color">*</span>
+                <input type="text" id="product_color" name="product_color" placeholder="Ex. red" value="<?php echo htmlspecialchars($product_color);?>" />
                 <br />
 
                 <label>Price:</label>
-                <input type="number" step="0.01" name="product_price" placeholder="100.00" value="<?php echo htmlspecialchars($product_price);?>">
+                <span id="price">*</span>
+                <input type="number" step="0.01" id="product_price" name="product_price" placeholder="100.00" value="<?php echo htmlspecialchars($product_price);?>">
                 <br />
                 <br />
 
-                <input type="submit" value="Submit">
+                <input id="submit" type="submit" value="Submit">
+
+                <input id="reset_button" type="reset" value="Clear">
 
             </form>
 
             
-            <form id="clear" action="create_form.php" method="post">
+            <!--<form id="clear" action="create_form.php" method="post">
                 <label>Clear Form:</label>
                 <br />
                 <input type="submit" value="Clear">
-            </form>
+            </form>-->
 
         </main>
+        <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+        <script>
+            $(document).ready ( () => {
+                $("#createform").submit ( (event) => {
+                    // code
+                    if($("#product_code").val() == '') {
+                        event.preventDefault();
+                        $("#code").text("Code cannot be blank");
+                    } else if ($("#product_code").val().length < 4 || $("#product_code").val().length > 10){
+                        event.preventDefault();
+                        $("#code").text("Code has to be >= 4 or <= 10 characters");
+                    }
+                    // name
+                    if($("#product_name").val() == '') {
+                        event.preventDefault();
+                        $("#name").text("Name cannot be blank");
+                    } else if ($("#product_name").val().length < 10 || $("#product_name").val().length > 100){
+                        event.preventDefault();
+                        $("#name").text("Name has to be >= 10 or <= 100 characters");
+                    } 
+                    // description
+                    if($("#description").val() == '') {
+                        event.preventDefault();
+                        $("#text").text("Description cannot be blank");
+                    } else if ($("#description").val().length < 10 || $("#description").val().length > 255){
+                        event.preventDefault();
+                        $("#text").text("Description has to be >= 10 or <= 255 characters");
+                    } 
+                    // color
+                    if($("#product_color").val() == '') {
+                        event.preventDefault();
+                        $("#color").text("Color cannot be blank");
+                    } else if ($("#product_color").val().length < 1 || $("#product_color").val().length > 20){
+                        event.preventDefault();
+                        $("#color").text("Color has to be >= 10 or <= 20 characters");
+                    } 
+                    // price
+                    if($("#product_price").val() == '') {
+                        event.preventDefault();
+                        $("#price").text("Price cannot be blank");
+                    } else if ($("#product_price").val() <= 0 || $("#product_price").val() > 100000){
+                        event.preventDefault();
+                        $("#price").text("Price should not be <= $0 or > $100,000");
+                    } 
+                });
+
+                $("#reset_button").click( () => {
+                    $("#product_code").val("");
+                    $("#code").text("*");
+
+                    $("#product_name").val("");
+                    $("#name").text("*");
+
+                    $("#description").val("");
+                    $("#text").text("*");
+
+                    $("#product_color").val("");
+                    $("#color").text("*");
+
+                    $("#product_price").val("");
+                    $("#price").text("*");
+                }); 
+            });            
+        </script>
         <?php include('footer.php');?>
     </body>
 </html>
